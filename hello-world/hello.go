acb75e274e247affdd055a079768b5f2ff682b45
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2024-07-12 15:43:58 +0300

    done hello world chapter

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,10 +2,40 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, world"
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+// here we have named prefix which is NAMED RETURN which means a variable is by that name is accessible with default value to 0, "", based on type
+// it doesn't need to be mentioned explicitly in return 
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world", ""))
 }
+
+// public function start with uppercase i.e Hello() - it is accessible from all files in the modules
+// private function start with lowercase i.e greetingPrefix()- not accessible from other files in the modules
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -2,10 +2,37 @@ package main
 
 import "testing"
 
+// test file needs to be postfixed xyz_test.go
+// test function needs to prefixed by TestXYZ(t *testing.T)
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("support Spanish", func (t *testing.T){
+		got := Hello("Remon", "Spanish")
+		want := "Hola, Remon"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("support French", func (t *testing.T){
+		got := Hello("Remon", "French")
+		want := "Bonjour, Remon"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // when test fails, it will not give line number of this assertion function because we have declared it as a helper like this
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
